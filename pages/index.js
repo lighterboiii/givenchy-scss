a10b55e119256731a703b1dca33e4855985332bc
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2022-08-26 15:32:32 +0300

    feature: написал скрипт для прокрутки к элементу по клику на кнопку в header

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+const headerButton = document.querySelector('.header__subscribe-button');
+const footerInput = document.getElementById('email');
+
+headerButton.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    footerInput.scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+// реализована прокрутка до footer__input по клику на header__subscribe-button
