3df66d0d69df824122eef9840f3d7c568dab3761
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2022-09-01 11:35:12 +0300

    feat: анимация fade in

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,32 @@
-// реализована прокрутка до footer__input по клику на header__subscribe-button
+// открытие/закрытие попапа меню
+const popup = document.querySelector('.header__popup');
+const openButton = document.querySelector('.header__button');
+const closeButton = document.querySelector('.header__close-button');
 
+openButton.addEventListener('click', function (evt) {
+    evt.preventDefault;
+    popup.classList.add('header__popup_type_opened');
+})
+
+closeButton.addEventListener('click', function (evt) {
+    evt.preventDefault;
+    popup.classList.remove('header__popup_type_opened');
+})
+
+// перелистывание по якорю
+const aboutButton = document.querySelector('.about');
+const looksButton = document.querySelector('.look');
+const footerButton = document.querySelector('.foot');
 const headerButton = document.querySelector('.header__subscribe-button');
+
+const aboutSection = document.getElementById('about');
+const looksSection = document.getElementById('looks');
+const footerSection = document.getElementById('footer');
 const footerInput = document.getElementById('email');
+
+scrollTo(aboutButton, aboutSection);
+scrollTo(looksButton, looksSection);
+scrollTo(footerButton, footerSection);
 
 headerButton.addEventListener('click', function (evt) {
     evt.preventDefault();
@@ -12,35 +37,6 @@ headerButton.addEventListener('click', function (evt) {
     footerInput.select();
 });
 
-// открытие/закрытие попапа меню
-const popup = document.querySelector('.header__popup');
-const openButton = document.querySelector('.header__button');
-const closeButton = document.querySelector('.header__close-button');
-
-openButton.addEventListener('click', function(evt) {
-  evt.preventDefault;
-  popup.classList.add('header__popup_type_opened');
-})
-
-closeButton.addEventListener('click', function(evt) {
-  evt.preventDefault;
-  popup.classList.remove('header__popup_type_opened');
-})
-
-// перелистывание по клику на ссылку в попапе
-const aboutButton = document.querySelector('.about');
-const looksButton = document.querySelector('.look');
-const footerButton = document.querySelector('.foot');
-
-const aboutSection = document.getElementById('about');
-const looksSection = document.getElementById('looks');
-const footerSection = document.getElementById('footer');
-
-scrollTo(aboutButton, aboutSection);
-scrollTo(looksButton, looksSection);
-scrollTo(footerButton, footerSection);
-
-
 function scrollTo(button, section) {
     button.addEventListener('click', function (evt) {
         evt.preventDefault();
@@ -48,11 +44,11 @@ function scrollTo(button, section) {
             behavior: 'smooth',
             block: 'start'
         });
-        });
+    });
 };
 
 // смена картинок по таймеру в блоке looks 
-let imgArray = ['looks-1.jpg', 'looks-2.jpg', 'looks-3.jpg', 'looks-4.jpg', 'looks-5.jpg', 'looks-6.jpg', 'looks-7.jpg', 'looks-8.jpg', 'looks-9.jpeg', 'looks-10.jpeg', 'looks-11.webp'];
+let imgArray = ['looks-1.jpg', 'looks-2.jpg', 'looks-3.jpg', 'looks-4.jpg', 'looks-5.jpg', 'looks-6.jpg', 'looks-7.jpg', 'looks-8.jpg', 'looks-9.jpeg', 'looks-10.jpeg', 'looks-11.jpeg', 'looks-12.jpeg'];
 const basePath = "./images/";
 const imageList = document.querySelector('.looks__images-list');
 const images = imageList.querySelectorAll('.looks__image');
@@ -67,12 +63,8 @@ window.setInterval(function () {
     let randomValueOfImages = randomInteger(1, 3);
     for (let i = 1; i <= randomValueOfImages; i++) {
         const randomImage = imgArray[Math.floor(Math.random() * imgArray.length)]
-            if (imagesArray[i].src === basePath + randomImage) {
-                continue;
-            } else {
-                randImg = images[Math.floor(Math.random() * images.length)]
-                randImg.src = basePath + randomImage;
-            }
+            randImg = images[Math.floor(Math.random() * images.length)]
+            randImg.src = basePath + randomImage;
     }
 }, 1000)
 
@@ -93,5 +85,3 @@ window.setInterval(function () {
 //         }
 //     }, 1000) 
 // }
-
-
